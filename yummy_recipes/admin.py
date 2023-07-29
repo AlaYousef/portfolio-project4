83c31c1b5f1e54e15b364fb1d3e3ffbeffc774fc
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-07-29 20:14:15 +0000

    add slug on title

--- yummy_recipes/admin.py
+++ yummy_recipes/admin.py
@@ -6,6 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    """Allows admin to manage recipes via the admin panel"""
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('description', 'ingredients', 'steps')
-
-# Register your models here.
