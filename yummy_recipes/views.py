69a8b8365aa930b7a5e968d1897a073769bd80fb
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-08-01 21:43:50 +0000

    style coctailes.html, add data to recipe model

--- yummy_recipes/views.py
+++ yummy_recipes/views.py
@@ -5,14 +5,13 @@ from .models import Recipe
 class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    
     template_name = 'index.html'
     paginate_by = 6
 
 
 class CoctailesList(generic.ListView):
     model = Recipe
-    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    queryset = Recipe.objects.filter(status=1, category=2).order_by('-created_on')
     
     template_name = 'coctailes.html'
     paginate_by = 6
